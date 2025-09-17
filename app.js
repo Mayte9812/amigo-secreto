041f4a12c1a274e2d0c583d2881d1195af5c77ca
Teresa Gomez <maria.gomeza17@tectijuana.edu.mx>
2025-09-17 13:42:07 -0700

    az

--- app.js
+++ app.js
@@ -1,3 +1,65 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+let amigosMostrados = [];
 
-let amigos =[];
+const nombres = document.querySelector('#amigo');
+const lista = document.querySelector('#listaAmigos');
+const resultado = document.getElementById('resultado');
+
+function agregarAmigo(){
+   if(nombres.value.trim() === ''){
+        alert('Por favor, ingresa un nombre válido.');
+        return;
+    }
+    else if(amigos.some(amigo => amigo.toLowerCase() === nombres.value.trim().toLowerCase()))  {
+        alert('Este nombre ya está en la lista.');
+        return; 
+ }       
+   else {
+        adherirLista();
+   }
+
+}
+
+function adherirLista(){
+     amigos.push(nombres.value.trim());
+        nombres.value = ''; 
+        console.log(amigos);
+        MostrarLista(amigos)
+        return;
+        
+}
+function MostrarLista(amigos){
+    lista.innerHTML = '';
+    amigos.forEach(amigo => {
+        const liAmigo = document.createElement('li');
+        liAmigo.textContent = amigo;
+        lista.appendChild(liAmigo);
+    });              
+    return;
+}
+function sortearAmigo(){
+    if(amigos.length < 2){
+        alert('Necesitas al menos 2 amigos para realizar el sorteo.');
+        return;
+    }   
+       const amigosRestantes = amigos.filter(amigo => !amigosMostrados.includes(amigo));
+    if(amigosRestantes.length === 0){
+        resultado.textContent = 'Ya todos han sido sorteados.';
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigosRestantes.length);
+    const secreto = amigosRestantes[indiceAleatorio];
+
+    amigosMostrados.push(secreto); 
+    resultado.textContent = `El amigo secreto es: ${secreto}`;
+    return;
+}
+function reiniciarSorteo(){
+    amigos = [];
+    amigosMostrados = [];
+    lista.innerHTML = '';
+    resultado.textContent = '';
+    nombres.value = '';
+    return;
+}   
